cf51a66da9b943d4a49c5614e2d636eeedc6b3a9
jtdoherty <161648987+jtdoherty@users.noreply.github.com>
2024-10-03 18:00:35 -0400

    Delete data.js

--- data.js
+++
@@ -1,5 +0,0 @@
-import advantagesDataRaw from './output7.json';
-
-const advantagesData = Array.isArray(advantagesDataRaw) ? advantagesDataRaw : [advantagesDataRaw];
-
-export default advantagesData;
